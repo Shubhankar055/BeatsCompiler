c4efe774cf87e7f6fba329c1346e8b6bba331a31
Shubhankar Shubham <44786799+Shubhankar055@users.noreply.github.com>
2019-01-01 20:25:17 +0530

    Update beat_finder.py

--- beat_finder.py
+++ beat_finder.py
@@ -1,8 +1,6 @@
 import librosa, librosa.display # Returns array of timestamps of beat
 def beat_dect(audio):
-    lst = []
     x, sr = librosa.load(audio)
-    ipd.Audio(x, rate=sr)
     tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=120, units='time')
     
 
