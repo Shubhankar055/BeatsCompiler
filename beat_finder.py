8bc2ba1334155e8494851b27070f53e041ebad6e
Shubhankar Shubham <44786799+Shubhankar055@users.noreply.github.com>
2019-01-01 20:54:53 +0530

    Update beat_finder.py
    Updated for
    Making the program short
    Returns the timestamps in normal list instead of array

--- beat_finder.py
+++ beat_finder.py
@@ -1,8 +1,10 @@
-import librosa, librosa.display # Returns array of timestamps of beat
+import librosa                                  # Updated for making program short and understandable
 def beat_dect(audio):
     x, sr = librosa.load(audio)
     tempo, beat_times = librosa.beat.beat_track(x, sr=sr, start_bpm=120, units='time')
+    lst = beat_times.tolist()                   # Converts the array into a simple list
+    rnd_lst = [round(items ,3) for items in lst]# For rounding off float upto 3 digits
     
-
-    x = beat_times
-    return(x)
+    
+    return(rnd_lst)
+    
